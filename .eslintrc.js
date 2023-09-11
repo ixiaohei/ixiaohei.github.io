c0823873288336eb3949ff44e189728234c77e42
kalvinzhang <kalvinzhang@futunn.com>
2023-09-11 18:14:29 +0800

    fix eslint support ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
